f859a079bd8ee0761858dfc11ea2bc3ac3c6d34f
JGil <jakegillespieemail@gmail.com>
2023-12-01 15:28:21 -0700

    edit route

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({extended:true}));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "/index.html"));
+    res.sendFile(path.join(__dirname + "views/index.ejs"));
 })
