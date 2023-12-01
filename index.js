11900aa8f917cf1d7c4fded4535311fb84952304
JGil <jakegillespieemail@gmail.com>
2023-12-01 15:32:38 -0700

    minor

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ let app = express();
 
 const port = 3000;
 
-app.set("views", path.join(__dirname, "../myviews"));
+app.set("views", path.join(__dirname, "/views"));
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({extended:true}));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "views/index.ejs"));
+    res.sendFile(path.join(__dirname + "/index.ejs"));
 })
