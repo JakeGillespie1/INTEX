169d0ce1ecb161b855867f49bc40ebc11d3237c0
JGil <jakegillespieemail@gmail.com>
2023-12-01 15:14:15 -0700

    App

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const express = require("express");
 
 let app = express();
+
+const port = 3000;
+
+/*this makes the body attribute visible in the request*/
+app.use(express.urlencoded({extended:true}));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/index.html"));
+})
