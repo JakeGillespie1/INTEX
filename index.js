6097404721be334e0804332f23fa22191e66cda9
jakerg@byu.edu <jakerg@byu.edu>
2023-12-02 14:01:06 -0700

    res.render

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.ejs'));
+    res.render(path.join(__dirname + '/index.ejs'));
 });
 
 app.listen(port, () => console.log('I am listening'));
