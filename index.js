87814975449892327e194e9b724898587389b7e4
jakerg@byu.edu <jakerg@byu.edu>
2023-12-02 13:59:41 -0700

    duplicate urlencoded

--- index.js
+++ index.js
@@ -6,18 +6,16 @@ let path = require('path');
 
 const port = 3000;
 
-app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(path.join(__dirname + '/public')));
 
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, '/views'));
 
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/views/index.ejs'));
+    res.sendFile(path.join(__dirname + '/index.ejs'));
 });
 
 app.listen(port, () => console.log('I am listening'));
