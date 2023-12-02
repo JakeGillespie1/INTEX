eb1200a28d2c5394b25dd66a50c6c5a40ab2b792
jakerg@byu.edu <jakerg@byu.edu>
2023-12-02 13:51:15 -0700

    change path to views

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.ejs'));
+    res.sendFile(path.join(__dirname + '/views/index.ejs'));
 });
 
 app.listen(port, () => console.log('I am listening'));
