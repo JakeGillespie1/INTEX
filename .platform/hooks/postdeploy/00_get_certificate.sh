580c96aaa0fc3fbcf96a1cde52688611a277d622
Gabriel Larson <gabrieljosephlarson@gmail.com>
2023-12-04 08:45:35 -0700

    added a record

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot -n -d intexfall23-env.eba-ixb2fd32.us-west-1.elasticbeanstalk.com --nginx --agree-tos --email gabrieljosephlarson@gmail.com
+sudo certbot -n -d socialmediaforprovo.is404.net --nginx --agree-tos --email gabrieljosephlarson@gmail.com
