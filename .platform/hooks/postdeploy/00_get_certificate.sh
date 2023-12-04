d4c4ea630cbb39abf2f0d1387eb3e0b542acb3ff
Gabriel Larson <gabrieljosephlarson@gmail.com>
2023-12-04 14:56:03 -0700

    yep

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 sudo certbot -n -d socialmediaforprovo.is404.net --nginx --agree-tos --email gabrieljosephlarson@gmail.com
 sudo certbot -n -d protectingprovo.is404.net --nginx --agree-tos --email gabrieljosephlarson@gmail.com
+sudo certbot -n -d mentalhealthforprovo.is404.net --nginx --agree-tos --email gabrieljosephlarson@gmail.com
+sudo certbot -n -d provo.is404.net --nginx --agree-tos --email gabrieljosephlarson@gmail.com
