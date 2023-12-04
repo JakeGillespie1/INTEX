c0e6c6ef439cc72e2a285e8aaf5984dd7d91fa1e
Gabriel Larson <gabrieljosephlarson@gmail.com>
2023-12-04 08:56:18 -0700

    adding second certificate

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 sudo certbot -n -d socialmediaforprovo.is404.net --nginx --agree-tos --email gabrieljosephlarson@gmail.com
+sudo certbot -n -d protectingprovo.is404.net --nginx --agree-tos --email gabrieljosephlarson@gmail.com
