49954607898a8126ad08f64f12db6468f711e125
Gabriel Larson <gabrieljosephlarson@gmail.com>
2023-12-03 22:22:53 -0700

    fixed issue

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d intexfall23-env.eba-ixb2fd32.us-west-1.elasticbeanstalk.com/ --nginx --agree-tos --email gabrieljosephlarson@gmail.com
+sudo certbot -n -d intexfall23-env.eba-ixb2fd32.us-west-1.elasticbeanstalk.com --nginx --agree-tos --email gabrieljosephlarson@gmail.com
