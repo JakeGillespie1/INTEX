929f5b381605daddcdd9374df2c19c55dc9c6569
ltsubaki <lilian.tsubaki@gmail.com>
2023-12-06 15:18:20 -0700

    Index.js work Lili

--- index.js
+++ index.js
@@ -114,6 +114,28 @@ app.post('/addRecord', (req, res) => {
        dbUseSM = true;
     else if (req.body.UseSM == 'N')
         dbUseSM = false;
+
+    //I skipped organizaiton becuase you can select multiple lolz I have no idea
+
+    //Occupation processing
+    if (req.body.avgTime == 'less')
+       dbMin = 0;
+       dbMax = 1;
+    else if (req.body.avgTime == 'oneTwo')
+        dbMin = 1;
+        dbMax = 2;
+    else if (req.body.avgTime == 'twoThree')
+        dbMin = 2;
+        dbMax = 3;
+    else if (req.body.avgTime == 'threeFour')
+        dbMin = 3;
+        dbMax = 4;
+    else if (req.body.avgTime == 'fourFive')
+        dbMin = 4;
+        dbMax = 5;
+    else if (req.body.avgTime == 'more')
+        dbMin = 5;
+        dbMax = null;
     
     knex('record')
         .insert({
