1d31fc3a4eb38d93242b54ea08ddfea9a5427061
ltsubaki <lilian.tsubaki@gmail.com>
2023-12-06 18:23:38 -0700

    added quotes

--- index.js
+++ index.js
@@ -216,7 +216,7 @@ app.post('/addRecord', (req, res) => {
         let currSocials = req.body.socialmediatypes || [];
 
         for (let iCount = 0; iCount < currSocials.length; iCount++) {
-        knex(platform_response)
+        knex('platform_response')
             .insert ({
                 platform_id: currSocials[iCount],
                 response_id: responseID,
